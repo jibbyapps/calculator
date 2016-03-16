d7da3b2815793dd654900fe667dc5af0f873566c
vitaliiromanchenko <packman2234@gmail.com>
2016-03-16 22:19:02 +0200

    calculator files

--- src/gui.java
+++ src/gui.java
@@ -8,7 +8,7 @@ import java.awt.event.*;
 public class gui extends JFrame {
 
     private  JTextField answerfield;
-    private JButton one,two, three, four, five, six, seven, eight, nine, zero, add, sub, multiply, div, equals, clear, decimial;
+    private JButton one,two, three, four, five, six, seven, eight, nine, zero, add, sub, multiply, div, equals, clear;
     private String stemp1,stemp2,sanswer ; //temporary variables;
     private double answer = 0.0;
     private JPanel contentPanel;
@@ -59,7 +59,7 @@ public class gui extends JFrame {
         eight.addActionListener(n);
         nine.addActionListener(n);
         zero.addActionListener(n);
-        decimial.addActionListener(n);
+        //decimial.addActionListener(n);
 
         add.addActionListener(c);
         div.addActionListener(c);
@@ -78,7 +78,8 @@ public class gui extends JFrame {
         contentPanel.add(one);contentPanel.add(two);contentPanel.add(three);contentPanel.add(four);contentPanel.add(five);
         contentPanel.add(six);contentPanel.add(seven);contentPanel.add(eight);contentPanel.add(nine);contentPanel.add(zero);
         contentPanel.add(add);contentPanel.add(div);contentPanel.add(sub);contentPanel.add(multiply);contentPanel.add(equals);
-        contentPanel.add(clear);contentPanel.add(decimial);
+        contentPanel.add(clear);
+        //contentPanel.add(decimial);
 
 
         contentPanel.setLayout(new FlowLayout());
@@ -89,46 +90,42 @@ public class gui extends JFrame {
     }
 
     private class  Numbers implements ActionListener{
-        public void actionPerformed(ActionEvent event){
+        public void actionPerformed(ActionEvent event) {
             JButton src = (JButton) event.getSource();
-            if (src.equals(one)){
-                if (opChosen ==false){
-                      if (stemp1==null){
+            if (src.equals(one)) {
+                if (opChosen == false) {
+                    if (stemp1 == null) {
                         stemp1 = "1";
 
-                       }
-                    else {
-                        stemp1 = stemp1 +1;
+                    } else {
+                        stemp1 = stemp1 + 1;
                     }
                 } else {
-                    if (stemp2==null){
+                    if (stemp2 == null) {
                         stemp2 = "1";
 
-                    }
-                    else {
-                        stemp2 = stemp2 +1;
+                    } else {
+                        stemp2 = stemp2 + 1;
 
 
                     }
                 }
             }
 
-            if (src.equals(two)){
-                if (opChosen ==false){
-                    if (stemp1==null){
+            if (src.equals(two)) {
+                if (opChosen == false) {
+                    if (stemp1 == null) {
                         stemp1 = "2";
 
-                    }
-                    else {
-                        stemp1 = stemp1 +2;
+                    } else {
+                        stemp1 = stemp1 + 2;
                     }
                 } else {
-                    if (stemp2==null){
+                    if (stemp2 == null) {
                         stemp2 = "2";
 
-                    }
-                    else {
-                        stemp2 = stemp2 +2;
+                    } else {
+                        stemp2 = stemp2 + 2;
 
 
                     }
@@ -136,88 +133,80 @@ public class gui extends JFrame {
             }
 
 
-            if (src.equals(three)){
-                if (opChosen ==false){
-                    if (stemp1==null){
+            if (src.equals(three)) {
+                if (opChosen == false) {
+                    if (stemp1 == null) {
                         stemp1 = "3";
 
-                    }
-                    else {
-                        stemp1 = stemp1 +3;
+                    } else {
+                        stemp1 = stemp1 + 3;
                     }
                 } else {
-                    if (stemp2==null){
+                    if (stemp2 == null) {
                         stemp2 = "3";
 
-                    }
-                    else {
-                        stemp2 = stemp2 +3;
+                    } else {
+                        stemp2 = stemp2 + 3;
 
 
                     }
                 }
             }
 
-            if (src.equals(four)){
-                if (opChosen ==false){
-                    if (stemp1==null){
+            if (src.equals(four)) {
+                if (opChosen == false) {
+                    if (stemp1 == null) {
                         stemp1 = "4";
 
-                    }
-                    else {
-                        stemp1 = stemp1 +4;
+                    } else {
+                        stemp1 = stemp1 + 4;
                     }
                 } else {
-                    if (stemp2==null){
+                    if (stemp2 == null) {
                         stemp2 = "4";
 
-                    }
-                    else {
-                        stemp2 = stemp2 +4;
+                    } else {
+                        stemp2 = stemp2 + 4;
 
 
                     }
                 }
             }
 
-            if (src.equals(five)){
-                if (opChosen ==false){
-                    if (stemp1==null){
+            if (src.equals(five)) {
+                if (opChosen == false) {
+                    if (stemp1 == null) {
                         stemp1 = "5";
 
-                    }
-                    else {
-                        stemp1 = stemp1 +5;
+                    } else {
+                        stemp1 = stemp1 + 5;
                     }
                 } else {
-                    if (stemp2==null){
+                    if (stemp2 == null) {
                         stemp2 = "5";
 
-                    }
-                    else {
-                        stemp2 = stemp2 +5;
+                    } else {
+                        stemp2 = stemp2 + 5;
 
 
                     }
                 }
             }
 
-            if (src.equals(six)){
-                if (opChosen ==false){
-                    if (stemp1==null){
+            if (src.equals(six)) {
+                if (opChosen == false) {
+                    if (stemp1 == null) {
                         stemp1 = "6";
 
-                    }
-                    else {
-                        stemp1 = stemp1 +6;
+                    } else {
+                        stemp1 = stemp1 + 6;
                     }
                 } else {
-                    if (stemp2==null){
+                    if (stemp2 == null) {
                         stemp2 = "6";
 
-                    }
-                    else {
-                        stemp2 = stemp2 +6;
+                    } else {
+                        stemp2 = stemp2 + 6;
 
 
                     }
@@ -225,88 +214,80 @@ public class gui extends JFrame {
             }
 //
 
-            if (src.equals(seven)){
-                if (opChosen ==false){
-                    if (stemp1==null){
+            if (src.equals(seven)) {
+                if (opChosen == false) {
+                    if (stemp1 == null) {
                         stemp1 = "7";
 
-                    }
-                    else {
-                        stemp1 = stemp1 +7;
+                    } else {
+                        stemp1 = stemp1 + 7;
                     }
                 } else {
-                    if (stemp2==null){
+                    if (stemp2 == null) {
                         stemp2 = "7";
 
-                    }
-                    else {
-                        stemp2 = stemp2 +7;
+                    } else {
+                        stemp2 = stemp2 + 7;
 
 
                     }
                 }
             }
 
-            if (src.equals(eight)){
-                if (opChosen ==false){
-                    if (stemp1==null){
+            if (src.equals(eight)) {
+                if (opChosen == false) {
+                    if (stemp1 == null) {
                         stemp1 = "8";
 
-                    }
-                    else {
-                        stemp1 = stemp1 +8;
+                    } else {
+                        stemp1 = stemp1 + 8;
                     }
                 } else {
-                    if (stemp2==null){
+                    if (stemp2 == null) {
                         stemp2 = "8";
 
-                    }
-                    else {
-                        stemp2 = stemp2 +8;
+                    } else {
+                        stemp2 = stemp2 + 8;
 
 
                     }
                 }
             }
 
-            if (src.equals(nine)){
-                if (opChosen ==false){
-                    if (stemp1==null){
+            if (src.equals(nine)) {
+                if (opChosen == false) {
+                    if (stemp1 == null) {
                         stemp1 = "9";
 
-                    }
-                    else {
-                        stemp1 = stemp1 +9;
+                    } else {
+                        stemp1 = stemp1 + 9;
                     }
                 } else {
-                    if (stemp2==null){
+                    if (stemp2 == null) {
                         stemp2 = "9";
 
-                    }
-                    else {
-                        stemp2 = stemp2 +9;
+                    } else {
+                        stemp2 = stemp2 + 9;
 
 
                     }
                 }
             }
 
-            if (src.equals(zero)){
-                if (opChosen ==false){
-                    if (stemp1==null){
+            if (src.equals(zero)) {
+                if (opChosen == false) {
+                    if (stemp1 == null) {
                         stemp1 = "0";
 
-                    }
-                    else {
-                        stemp1 = stemp1 +0;
+                    } else {
+                        stemp1 = stemp1 + 0;
                     }
                 } else {
-                    if (stemp2==null){
+                    if (stemp2 == null) {
                         stemp2 = "0";
 
-                    }
-                    else {
-                        stemp2 = stemp2 +0;
+                    } else {
+                        stemp2 = stemp2 + 0;
 
 
                     }
@@ -314,12 +295,12 @@ public class gui extends JFrame {
             }
 
 
-            if (src.equals(decimial)){
-                if (opChosen ==false){
-                    if (stemp1==null){
+         /*   if (src.equals(decimial)) {
+                if (opChosen == false)
+                    if (stemp1 == null) {
                         stemp1 = "0.";
 
-                    }else {
+                    } else {
                         if (stemp1 != null) {
                             if (stemp1.contains(".")) {
                                 System.out.println("you already have decimal!!!");
@@ -328,19 +309,24 @@ public class gui extends JFrame {
                             }
 
                         }
-                    }else {
-                        if(stemp2==null){
-                            stemp2="0.";
-                        } else
-                            if(stemp2.contains(".")){
-                                System.out.println("you already have a decimal point");
-                            }else {
-                                stemp2+=".";
-                            }
                     }
 
-                }
+                else{
+                        if (stemp2 == null) {
+                            stemp2 = "0.";
+                        } else if (stemp2.contains(".")) {
+                            System.out.println("you already have a decimal point");
+                        } else {
+                            stemp2 += ".";
+                        }
+                    }
+
+
+
             }
+
+            */
+
 
 
             if (equalsClicked ==false){
